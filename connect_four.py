0716a6cc3ec77a1df81ea1159ef8ba3e468804f7
Peter Warwick <peterwarwick10@gmail.com>
2025-08-14 10:39:23 +0100

    Used constants and tweaked window size

--- connect_four.py
+++ connect_four.py
@@ -1,9 +1,13 @@
 from tkinter import *
 import tkinter as tk
 
-Yellow = 1
-Red = 2
+YELLOW = 1
+RED = 2
 #setting the value for the disc colour, for use in the board
+
+ROWS = 6
+COLUMNS = 7
+CELL_SIZE = 100
 
 global turns
 turns = 0
@@ -11,49 +15,52 @@ turns = 0
 def playchange():
     global player
     if turns%2 == 0:
-        player = Yellow
+        player = YELLOW
     elif turns%2 == 1:
-        player = Red
+        player = RED
 playchange()
-board = [[0 for i in range(7)] for i in range(6)]
+board = [[0 for i in range(COLUMNS)] for i in range(ROWS)]
 
 
 
 def PvP_start():
     navigationMenuWin.destroy()
 
+    window_width = (COLUMNS*CELL_SIZE)+100
+    window_height = (ROWS*CELL_SIZE)+150
+
     PvPWin = Tk()
     PvPWin.title("Connect Four PvP")
-    PvPWin.geometry("800x800")
+    PvPWin.geometry(str(window_width)+"x"+str(window_height))
     PvPWin.configure(background="light blue")
 
     frame2 = Frame(PvPWin).pack()
     
 
-    canvas = tk.Canvas(PvPWin, width=700, height=600, bg="white")
+    canvas = tk.Canvas(PvPWin, width=(COLUMNS*CELL_SIZE), height=(ROWS*CELL_SIZE), bg="white")
     canvas.place(relx=0.5, rely=0.45, anchor="center")
 
-    #canvas.create_oval(100, 100, 0, 0, fill="red",) note for me
+    #canvas.create_oval(CELL_SIZE, CELL_SIZE, 0, 0, fill="RED",) note for me
 
-    def redraw():
-        for i in range(7):
-            canvas.create_line((100*i), 600, (100*i), 0, fill="black", width=3)
-        for i in range(6):
-            canvas.create_line(700, (100*i), 0, (100*i), fill="black", width=3)
+    def REDraw():
+        for i in range(COLUMNS):
+            canvas.create_line((CELL_SIZE*i), (ROWS*CELL_SIZE), (CELL_SIZE*i), 0, fill="black", width=3)
+        for i in range(ROWS):
+            canvas.create_line((COLUMNS*CELL_SIZE), (CELL_SIZE*i), 0, (CELL_SIZE*i), fill="black", width=3)
         
-        for column in range(7):
-            for row in range(6):
-                if board[row][column] == Red:
-                    canvas.create_oval(column*100, (6-row)*100, (column+1)*100, (5-row)*100, fill="red",)
-                elif board[row][column] == Yellow:
-                    canvas.create_oval(column*100, (6-row)*100, (column+1)*100, (5-row)*100, fill="yellow",)
+        for column in range(COLUMNS):
+            for row in range(ROWS):
+                if board[row][column] == RED:
+                    canvas.create_oval(column*CELL_SIZE, (ROWS-row)*CELL_SIZE, (column+1)*CELL_SIZE, ((ROWS-1)-row)*CELL_SIZE, fill="RED",)
+                elif board[row][column] == YELLOW:
+                    canvas.create_oval(column*CELL_SIZE, (ROWS-row)*CELL_SIZE, (column+1)*CELL_SIZE, ((ROWS-1)-row)*CELL_SIZE, fill="YELLOW",)
                     
     def drop(column):
-        for row in range(6):
+        for row in range(ROWS):
             if board[row][column] == 0:
                 board[row][column] = player
                 break
-        redraw()
+        REDraw()
         global turns
         turns += 1
         playchange()
@@ -62,7 +69,7 @@ def PvP_start():
     drop0.pack()
     
     
-    redraw()
+    REDraw()
     PvPWin.mainloop()
 
 
